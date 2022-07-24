2fc15da9d97c4e20c88f315de9d9d3ec049fdb37
Thipok Cholsaipant <thipok.ch@gmail.com>
2022-07-24 08:27:45 +0700

    ci: refactor gradle plugin config

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,11 +7,6 @@ buildscript {
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
         classpath("com.android.tools.build:gradle:7.2.1")
-        /** Tools **/
-        classpath("com.github.ben-manes:gradle-versions-plugin:0.42.0") // Dependency Update Notice
-        classpath("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.21.0") // Code Analysis
-        classpath("org.jlleitschuh.gradle.ktlint:org.jlleitschuh.gradle.ktlint.gradle.plugin:10.3.0") // Kotlin Analysis + Formatting
-        classpath("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:3.4.0.2513") // Code Analysis
     }
 }
 
